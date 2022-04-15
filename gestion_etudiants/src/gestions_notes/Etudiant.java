0b28e400c9ae9e907fdc899a492c9b21835d439b
hp <hp@192.168.56.1>
2022-04-15 16:51:08 +0000

    etudiant last update

--- gestion_etudiants/src/gestions_notes/Etudiant.java
+++ gestion_etudiants/src/gestions_notes/Etudiant.java
@@ -1,38 +1,48 @@
 package gestions_notes;
+
+//first part
 // declaration de variables;
-public class Etudiant implements Comparable <Etudiant> {
-private int id ;
-private String nom;
-private double note ;
+public class Etudiant implements Comparable<Etudiant> {
+	private int id;
+	private String nom;
+	private double note;
+	
+	public void setNote(double note) {
+		this.note = note;
+	}
+	public double getNote() {
+		return note;
+	}
+
 //creation de constructeur;
-Etudiant (int id , String nom)
-{this.id=id;
- this.nom=nom;
- this.note= 10;
-}
+	Etudiant(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+		this.note = 10;
+	}
+
 //redefinition de la methode to string;
-public String toString 
-{
-	return (this.nom + ":"+this.note);
-}
+public String toString()
+	{
+		return (this.nom + ":" + this.note);
+	}
+
 // redefinition de la methode equals.
-public boolean equals(Etudiant etd)
-{ if (this.id==etd.id)
-	    return true;
-else return false;
-}
+	public boolean equals(Etudiant etd) {
+		if (this.id == etd.id)
+			return true;
+		else
+			return false;
+	}
+
 //implementation de l interface compare etudiant et cretion de la methode compare to;
-public int compareTo(Etudiant e) {
-	if(this.note > e.note )
-		return 1;
-	else if (this.note==e.note)
-	    return 0;
-	else 
-	return -1;
-}
-
-
-
-
+	public int compareTo(Etudiant e) {
+		if (this.note > e.note)
+			return 1;
+		else if (this.note == e.note)
+			return 0;
+		else
+			return -1;
+	}
 
 }
