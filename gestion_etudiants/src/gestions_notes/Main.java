0ffc0f559a3988f1cdcc19dd5cc810b17f026a80
hp <hp@192.168.56.1>
2022-04-15 16:50:27 +0000

    comit main

---
+++ gestion_etudiants/src/gestions_notes/Main.java
@@ -0,0 +1,21 @@
+package gestions_notes;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		Etudiant e1 = new Etudiant(1,"hamid");
+	Etudiant e2 = new Etudiant(2,"driss");
+	Etudiant e3 = new Etudiant(3,"youssef");
+	e1.setNote(15);
+	e2.setNote(12);
+	e3.setNote(16);
+	System.out.println(e1);
+	System.out.println(e2);
+	System.out.println(e3);
+	
+	}
+	
+
+}
