00ad776c590a1cb69d566b02f711a6448d744d60
hp <hp@192.168.56.1>
2022-04-15 17:04:35 +0000

    comit main  till question 3-d

--- gestion_etudiants/src/gestions_notes/Main.java
+++ gestion_etudiants/src/gestions_notes/Main.java
@@ -7,13 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		Etudiant e1 = new Etudiant(1,"hamid");
 	Etudiant e2 = new Etudiant(2,"driss");
-	Etudiant e3 = new Etudiant(3,"youssef");
+	Etudiant e3 = new Etudiant(1,"youssef");
 	e1.setNote(15);
 	e2.setNote(12);
 	e3.setNote(16);
-	System.out.println(e1);
-	System.out.println(e2);
-	System.out.println(e3);
+	
+	
+	liste_Etudiants mes_Etudiants= new liste_Etudiants();
+	mes_Etudiants.add(e1);
+	mes_Etudiants.add(e2);
+	mes_Etudiants.add(e3);
+	System.out.println(mes_Etudiants);
+	
+	
 	
 	}
 	
