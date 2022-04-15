ea208b169f6525301237e590fe02e52b632ae6af
hp <hp@192.168.56.1>
2022-04-15 15:11:51 +0000

    comit the first part

--- gestion_etudiants/src/gestions_notes/Etudiant.java
+++ gestion_etudiants/src/gestions_notes/Etudiant.java
@@ -1,8 +1,38 @@
 package gestions_notes;
-
-public class Etudiant {
+// declaration de variables;
+public class Etudiant implements Comparable <Etudiant> {
 private int id ;
 private String nom;
 private double note ;
+//creation de constructeur;
+Etudiant (int id , String nom)
+{this.id=id;
+ this.nom=nom;
+ this.note= 10;
+}
+//redefinition de la methode to string;
+public String toString 
+{
+	return (this.nom + ":"+this.note);
+}
+// redefinition de la methode equals.
+public boolean equals(Etudiant etd)
+{ if (this.id==etd.id)
+	    return true;
+else return false;
+}
+//implementation de l interface compare etudiant et cretion de la methode compare to;
+public int compareTo(Etudiant e) {
+	if(this.note > e.note )
+		return 1;
+	else if (this.note==e.note)
+	    return 0;
+	else 
+	return -1;
+}
+
+
+
+
 
 }
